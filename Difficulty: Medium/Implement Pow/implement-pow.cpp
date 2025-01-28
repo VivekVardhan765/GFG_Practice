7344b6b0236f2c16718610abd2a88c555b83d78a
VivekVardhan765 <vivekvardhankaranam765@gmail.com>
2025-01-29 00:37:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Pow/implement-pow.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    double power(double b, int e) {
+        if (e == 0) {
+            return 1;
+        }
+        if (e < 0) {
+            return 1 / power(b, -e);
+        }
+        double half = power(b, e / 2);
+        if (e % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * b;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    cout << fixed << setprecision(5);
+    int t;
+    cin >> t;
+    while (t--) {
+        double b;
+        cin >> b;
+        int e;
+        cin >> e;
+        Solution ob;
+        cout << ob.power(b, e) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
