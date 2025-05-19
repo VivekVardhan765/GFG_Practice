e110d3bc322e6424999df9704653ce11ff658b80
VivekVardhan765 <vivekvardhankaranam765@gmail.com>
2025-05-19 12:28:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.cpp
@@ -0,0 +1,180 @@
+//{ Driver Code Starts
+// C++ program to find predecessor and successor in a BST
+#include "bits/stdc++.h"
+using namespace std;
+
+// BST Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+
+/* BST Node
+class Node
+{
+   public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+    void fr(Node* root,int key,Node*& r){
+        if(!root)return;
+        if(root->data>key){
+            if(r->data > root->data){
+                r=root;
+                fr(root->left,key,r);
+            }
+        }
+        else if(root->data<key){
+            fr(root->right,key,r);
+        }
+        else{
+            fr(root->right,key,r);
+        }
+    }
+    void fl(Node* root,int key,Node*& l){
+        if(!root)return;
+        if(root->data<key){
+            if(l->data < root->data){
+                l=root;
+                fl(root->right,key,l);
+            }
+        }
+        else if(root->data>key){
+                fl(root->left,key,l);
+        }
+        else{
+            fl(root->left,key,l);
+        }
+    }
+    
+    vector<Node*> findPreSuc(Node* root, int key) {
+        Node* l = new Node(-1e7);
+        Node* r = new Node(1e7);
+
+        fl(root,key,l);
+        fr(root,key,r);
+        if(r->data==1e7)r->data=-1;
+        if(l->data==-1e7)l->data=-1;
+        return {l,r};
+    }
+};
+
+
+//{ Driver Code Starts.
+
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Driver program to test above functions
+int main() {
+
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        getline(cin, s);
+        int key = stoi(s);
+
+        Solution ob;
+        vector<Node*> result = ob.findPreSuc(root, key);
+        Node* pre = result[0];
+        Node* suc = result[1];
+
+        if (pre != nullptr)
+            cout << pre->data << " ";
+        else
+            cout << -1 << " ";
+
+        if (suc != nullptr)
+            cout << suc->data << endl;
+        else
+            cout << -1 << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
