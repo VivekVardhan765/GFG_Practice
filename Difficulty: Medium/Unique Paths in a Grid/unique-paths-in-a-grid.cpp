ed89acf73925c66e8f8fa50149cd243bdbf55923
VivekVardhan765 <vivekvardhankaranam765@gmail.com>
2025-06-03 14:25:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique Paths in a Grid/unique-paths-in-a-grid.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int uniquePaths(vector<vector<int>> &grid) {
+        // code here
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        if(grid[0][0]==1 or grid[n-1][m-1]==1)return 0;
+        dp[0][0]=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]!=1){
+                    if(i>0){
+                        dp[i][j]+=dp[i-1][j];
+                    }
+                    if(j>0){
+                        dp[i][j]+=dp[i][j-1];
+                    }
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
